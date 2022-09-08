3552de96b2bbc5e1d3169d49e7fb50e4020e6a51
Thomas Lonowski <thomaslonowski@u.boisestate.edu>
2022-09-07 21:38:41 -0600

    Commit 6: got the program working with some stuff in a function

--- VowelCount/vc.c
+++ VowelCount/vc.c
@@ -1,23 +1,35 @@
+/*
+ * Program that takes in user input via getline(), counts the number of vowels in the input,
+ * and prints this number to stdout. This process loops until an exit command is entered (Crtl+d)
+ *
+ * @author Thomas Lonowski
+ * @class CS 253
+ * @date 7 September 2022
+ */
+
 #include <stdio.h>
 
 const char vowels[] = "aeiou";
-const int vowelsArrayLength = sizeof(vowels) / sizeof(vowels[0]);	// length of the vowels[] array AKA the number of possible vowels (5 in this case: aeiou
+
 
 /**
-* @brief Takes in a char c and determines if it is lower case or upper case
-* @param char c, a standard character
-* @return int, 0 for upper case and 1 for lower case
-*/
-int isLower(char c) {
-	int isLower = 0;	// false
-	if(c >= 'a' && c <= 'z') {	// if element is lower case
-		isLower = 1;	// true		
+ * Takes in a char c and returns if it is upper case
+ *
+ * @param char c
+ * @return int, 0 for upper case and 1 for lower case
+ */
+int isUpperCase(char c) {
+	int isUpperCase = 0;	// "false"
+	if(c >= 'A' && c <= 'Z') {	// Determines if char is upper case
+		isUpperCase = 1;	// "true"
 	}
-	return isLower;
+	return isUpperCase;
 }
 
+
 /**
- * @brief Runs an input loop, counts the number of vowels in each input line, and prints it to the console
+ * Runs a loop that: collects user input via getline(), counts the number of vowels in the input, and prints it to stdout
+ * 
  * @return void
  */
 void countVowels() {
@@ -26,36 +38,31 @@ void countVowels() {
 	int vowelCount = 0;
 
 	while(1) {
-		ssize_t len = getline(&input, &size, stdin);	// Get user input
+		ssize_t len = getline(&input, &size, stdin);	// Get user input, storing input length in len
 
 		if(len < 0) { break; }	// while loop exit condition
 
-		// Change each char to upper case
 		for(int i = 0; i < len; i++) {
-			if(isLower(input[i])) {
-				input[i] -= 'A' - 'a';
-			}
+			if(isUpperCase(input[i])) { input[i] += 'a' - 'A'; }	// If an input char is upper case, change it to lower case
 		}
 
-		// // Take the user's input line and make every char lower case
-		// for(int i = 0; i < len; i++) {	// for every char in the user's input line
-		// 	if(line[i] >= 'A' && line[i] <= 'Z') {
-		// 		line[i] -= 'A' - 'a';
-		// 	}
-		// }
-
-		// Compare each char to the vowels[] array, incrementing vowelCount for each vowel match
+		// Compare each input char to the vowels[] array, incrementing vowelCount for each vowel found
 		for(int i = 0; i < len; i++) {
-			for(int k = 0; k < vowelsArrayLength; k++) {
+			for(size_t k = 0; k < sizeof(vowels); k++) {
 				if(input[i] == vowels[k]) { vowelCount++; }
 			}
 		}
 
-		printf("%d", vowelCount);
+		printf("%d\n", vowelCount);
 		vowelCount = 0;
 	}
 }
 
+/**
+ * Main function: calls countVowels() function
+ * 
+ * @return int 0, standard return value for main function
+ */
 int main() {
 	countVowels();
 	return 0;
